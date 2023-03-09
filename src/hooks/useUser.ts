6f6d6e9b387d81963315d66e84b5a05a77b724f7
Víctor Merino <victor.cornella.86@gmail.com>
2023-03-09 20:46:36 +0100

    Fix custom hook registerUser method to works with uiSlice

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,13 +1,42 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import {
+  openErrorFeedbackActionCreator,
+  openSuccessFeedbackActionCreator,
+} from "../redux/features/uiSlice/uiSlice";
+import { useAppDispatch } from "../redux/hooks";
 import { UserRegisterData } from "../types/types";
+import { AxiosResponseBody } from "./types";
 
 const useUser = () => {
   const apiUrl = process.env.REACT_APP_API_URL!;
+  const dispatch = useAppDispatch();
 
   const registerUser = async (userData: UserRegisterData) => {
     try {
       await axios.post(`${apiUrl}/users/sign-up`, userData);
+      dispatch(
+        openSuccessFeedbackActionCreator({
+          feedback: {
+            isOpen: true,
+            messageFeedback: `User ${userData.username} registered successfully`,
+            isError: false,
+          },
+          isLoading: false,
+        })
+      );
     } catch (error: unknown) {
+      dispatch(
+        openErrorFeedbackActionCreator({
+          feedback: {
+            isOpen: true,
+            messageFeedback: `It was not possible to register ${(
+              error as AxiosError<AxiosResponseBody>
+            ).response?.data.error!}`,
+            isError: true,
+          },
+          isLoading: false,
+        })
+      );
       throw new Error(
         `It was not possible to register: ${(error as Error).message}`
       );
