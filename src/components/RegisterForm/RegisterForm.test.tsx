e40e0ec297d2bc616a1ad71a306e8aa7832956f6
Víctor Merino <victor.cornella.86@gmail.com>
2023-03-20 22:52:06 +0100

    Test RegisterForm ui component submit function

--- src/components/RegisterForm/RegisterForm.test.tsx
+++ src/components/RegisterForm/RegisterForm.test.tsx
@@ -1,6 +1,13 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import ProviderWrapper from "../../mocks/providerWrapper";
 import RegisterForm from "./RegisterForm";
+
+const mockRegisterAction = jest.fn();
+
+jest.mock("../../hooks/useUser", () => {
+  return () => ({ registerUser: mockRegisterAction });
+});
 
 describe("Given the RegisterForm ui component", () => {
   describe("When it is rendered", () => {
@@ -18,6 +25,25 @@ describe("Given the RegisterForm ui component", () => {
 
       expect(usernameInput).toBeInTheDocument();
       expect(passwordInput).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's filled in and its button 'Register' is clicked", () => {
+    test("Then the form should be submited", async () => {
+      render(
+        <ProviderWrapper>
+          <RegisterForm />
+        </ProviderWrapper>
+      );
+
+      const usernameInput = screen.queryByRole("textbox", {
+        name: "Escribe tu nombre de usuario",
+      })!;
+      await userEvent.type(usernameInput, "Jowie");
+      const passwordInput = screen.queryByLabelText("Crea una contraseña")!;
+      await userEvent.type(passwordInput, "julito");
+      const button = screen.queryByRole("button")!;
+      await userEvent.click(button);
     });
   });
 });
