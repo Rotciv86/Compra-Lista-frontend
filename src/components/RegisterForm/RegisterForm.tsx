9e8457d22417295a194c1f37019a18a9f393205f
Víctor Merino <victor.cornella.86@gmail.com>
2023-03-20 19:47:30 +0100

    Create and add handleSubmit function in RegisterForm ui component

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import useUser from "../../hooks/useUser";
 import { UserRegisterData } from "../../types/types";
 import ButtonStyled from "../Button/ButtonStyled";
 import RegisterFormStyled from "./RegisterFormStyled";
@@ -10,44 +11,54 @@ const initialFormData: UserRegisterData = {
 
 const RegisterForm = (): JSX.Element => {
   const [formData, setFormData] = useState(initialFormData);
+  const { registerUser } = useUser();
 
   const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [event.target.id]: event.target.value });
   };
 
-  return (
-    <RegisterFormStyled>
-      <form action="" className="register-form">
-        <div className="register-form__container">
-          <div className="register-form__form-group">
-            <label htmlFor="username">Escribe tu nombre de usuario</label>
-            <input
-              className="register-form__input"
-              type="text"
-              id="username"
-              autoComplete="off"
-              onChange={handleFormChange}
-              value={formData.username}
-              required
-            />
-          </div>
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
 
-          <div className="register-form__form-group">
-            <label htmlFor="password">Crea una contraseña</label>
-            <input
-              className="register-form__input"
-              type="password"
-              id="password"
-              autoComplete="off"
-              minLength={4}
-              onChange={handleFormChange}
-              value={formData.password}
-              required
-            />
-          </div>
+    const formSubmitData: UserRegisterData = {
+      username: formData.username,
+      password: formData.password,
+    };
+
+    registerUser(formSubmitData);
+  };
+
+  return (
+    <RegisterFormStyled className="register-form" onSubmit={handleSubmit}>
+      <div className="register-form__container">
+        <div className="register-form__form-group">
+          <label htmlFor="username">Escribe tu nombre de usuario</label>
+          <input
+            className="register-form__input"
+            type="text"
+            id="username"
+            autoComplete="off"
+            onChange={handleFormChange}
+            value={formData.username}
+            required
+          />
         </div>
-        <ButtonStyled>Registrate</ButtonStyled>
-      </form>
+
+        <div className="register-form__form-group">
+          <label htmlFor="password">Crea una contraseña</label>
+          <input
+            className="register-form__input"
+            type="password"
+            id="password"
+            autoComplete="off"
+            minLength={4}
+            onChange={handleFormChange}
+            value={formData.password}
+            required
+          />
+        </div>
+      </div>
+      <ButtonStyled>Regístrate</ButtonStyled>
     </RegisterFormStyled>
   );
 };
