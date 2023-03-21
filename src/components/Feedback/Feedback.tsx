c3d5b5ec80cea8e5346c2b7462a25512901c87df
Víctor Merino <victor.cornella.86@gmail.com>
2023-03-21 13:09:20 +0100

    Create Feedback ui component

---
+++ src/components/Feedback/Feedback.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { closeFeedbackActionCreator } from "../../redux/features/uiSlice/uiSlice";
+import { useAppDispatch } from "../../redux/hooks";
+
+interface FeedbackProps {
+  isOpen: boolean;
+  messageFeedback: string;
+  isError: boolean;
+}
+
+const Feedback = ({ isError, isOpen, messageFeedback }: FeedbackProps) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        dispatch(closeFeedbackActionCreator());
+      });
+    }
+  }, [dispatch, isOpen]);
+
+  return <span className="feedback-text">{messageFeedback}</span>;
+};
+
+export default Feedback;
