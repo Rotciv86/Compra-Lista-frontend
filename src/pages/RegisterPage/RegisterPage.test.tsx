b6b90c9b36f09bfade5da2e979a5a75f477b8fbf
Víctor Merino <victor.cornella.86@gmail.com>
2023-03-28 15:51:31 +0200

    Test navigate to home page in NotFound page

--- src/pages/RegisterPage/RegisterPage.test.tsx
+++ src/pages/RegisterPage/RegisterPage.test.tsx
@@ -4,7 +4,7 @@ import RegisterPage from "./RegisterPage";
 
 describe("Given a RegisterPage", () => {
   describe("When its rendered", () => {
-    test("Then it should show a heading level 1 with text 'REGISTER'", () => {
+    test("Then it should show a heading level 2 with text 'REGISTER'", () => {
       const headingText = "¡BIENVENIDO A COMPRA LISTA, LA COMPRA MÁS FACIL!";
 
       renderWithProviders(<RegisterPage />);
