711c8ad2b726349dce90557da7b880279ba8db59
Víctor Merino <victor.cornella.86@gmail.com>
2023-03-30 19:44:36 +0200

    Create loginUser reducer and login action creator

---
+++ src/redux/features/userSlice/userSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserLoginData, UserState } from "../../../types/types";
+
+const userInitialState: UserState = {
+  username: "",
+  id: "",
+  token: "",
+  isLogged: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: userInitialState,
+  reducers: {
+    loginUser: (currentUserState, action: PayloadAction<UserLoginData>) => ({
+      ...action.payload,
+      isLogged: true,
+    }),
+  },
+});
+
+export const userReducer = userSlice.reducer;
+
+export const { loginUser: loginUserActionCreator } = userSlice.actions;
