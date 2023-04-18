3bcf1ed539d3a94d89621807a9bb663cb3321075
Víctor Merino <victor.cornella.86@gmail.com>
2023-04-18 17:37:42 +0200

    Add Loader ui component to Layout and position in it

--- src/components/Loader/LoaderStyled.tsx
+++ src/components/Loader/LoaderStyled.tsx
@@ -1,6 +1,16 @@
 import styled from "styled-components";
 
 const LoaderStyled = styled.span`
+  background-color: rgba(255, 255, 255, 0.7);
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  z-index: 1;
   .loader {
     position: relative;
     width: 120px;
